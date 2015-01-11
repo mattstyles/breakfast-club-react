1e46604ade3d7c02a91b1286b577c54dd6f32a46
mattstyles <matt@veryfizzyjelly.com>
2015-01-11 23:02:53 +0000

    #add - trying to request from bitbucket

--- src/scripts/stores/teamStore.js
+++ src/scripts/stores/teamStore.js
@@ -1,5 +1,6 @@
 
 var EventEmitter = require( 'events' ).EventEmitter;
+var request = require( 'superagent' );
 var constants = require( '../constants/actions.js' );
 var router = require( '../routers/router.jsx' );
 var dispatcher = require( '../dispatchers/dispatcher' );
@@ -26,22 +27,25 @@ class TeamStore extends EventEmitter {
     }
 
     _getMembers( id ) {
-        setTimeout( function() {
-            this.emit( constants.CHANGE_EVENT, {
-                foo: 'foo',
-                team: id,
-                members: [
-                    {
-                        foo: 'foo',
-                        bar: 'baz'
-                    },
-                    {
-                        foo: 'bar',
-                        bar: 'quux'
-                    }
-                ]
-            });
-        }.bind( this ), 1000 );
+        console.log( 'getting members', id );
+        request
+            .get( 'https://bitbucket.org/api/2.0/teams/' + id + '/members' )
+            .set('Content-Type', 'application/json; charset=utf-8')
+            .withCredentials()
+            .end( function( res ) {
+                if ( !res.ok ) {
+                    throw new Error( 'Error fulfilling request' );
+                }
+
+                console.log( res );
+
+                this.emit( constants.CHANGE_EVENT, {
+                    team: id,
+                    members: res
+                });
+            }.bind( this ));
+
+
     }
 
     get() {
